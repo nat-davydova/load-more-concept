74cd7d270eb1872a853393e43a8bccb53bb1a7b0
nat-davydova <natti.davydova@gmail.com>
2020-12-06 17:38:03 +0300

    load more: add event listener to load more btn

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -9,4 +9,12 @@ import { showElemsInitially } from "./modules/showElemsInitially";
   window.addEventListener("load", () => {
     showElemsInitially(PATH.cardItem, INIT_ELEMS_TO_SHOW);
   });
+
+  document.addEventListener("click", e => {
+    const target = e.target as HTMLElement;
+
+    if (target.closest(PATH.loadMoreBtn)) {
+      console.log("clicked");
+    }
+  });
 })();
