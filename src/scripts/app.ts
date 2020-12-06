af53210b7c3430af3242a850dda9b36ce1214d13
nat-davydova <natti.davydova@gmail.com>
2020-12-06 15:03:15 +0300

    load more: add linter, path and preps

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,1 +1,4 @@
-
+(function() {
+  const INIT_ELEMS_TO_SHOW = 3;
+  const ELEMS_TO_REVEAL = 3;
+})();
---
+++ src/scripts/configs/path.ts
@@ -0,0 +1,4 @@
+export const PATH = {
+  cardItem: ".card-block",
+  loadMoreBtn: ".load-more"
+};
