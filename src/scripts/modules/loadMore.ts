d0fc807f2dcfaab9f0912dbf612d37c91327d9b2
nat-davydova <natti.davydova@gmail.com>
2020-12-12 17:03:19 +0300

    load more: add scrolling

--- src/scripts/modules/loadMore.ts
+++ src/scripts/modules/loadMore.ts
@@ -1,11 +1,21 @@
 import { PATH } from "../configs/path";
 
-import { getElemsArray, hideElem, revealElems } from "./utils";
+import { getElemsArray, getElemOffset, hideElem, revealElems } from "./utils";
 
 function hideLoadMoreBtn(): void {
   const loadMoreBtn = document.querySelector<HTMLElement>(PATH.loadMore.btn);
 
   loadMoreBtn && hideElem(loadMoreBtn);
+}
+
+function revealHiddenElems(elems: HTMLElement[]): void {
+  revealElems(elems);
+
+  const elem1 = elems[0];
+
+  const offsetTop = getElemOffset(elem1, "top");
+
+  window.scrollTo({ top: offsetTop, behavior: "smooth" });
 }
 
 export function loadMore(elemsClassname: string, numToShow: number): void {
@@ -16,7 +26,7 @@ export function loadMore(elemsClassname: string, numToShow: number): void {
 
     const elemsToShow = hiddenElems.splice(0, numToShow);
 
-    revealElems(elemsToShow);
+    revealHiddenElems(elemsToShow);
 
     !hiddenElems.length && hideLoadMoreBtn();
   }
--- src/scripts/modules/utils.ts
+++ src/scripts/modules/utils.ts
@@ -2,6 +2,18 @@ export function getElemsArray(elemClassname: string): HTMLElement[] | null {
   return Array.from(document.querySelectorAll<HTMLElement>(elemClassname));
 }
 
+type Direction = "top" | "left";
+
+export function getElemOffset(elem: HTMLElement, direction: Direction): number {
+  const rect = elem.getBoundingClientRect();
+
+  if (direction === "top") {
+    return rect.top + window.scrollY;
+  } else {
+    return rect.left + window.scrollX;
+  }
+}
+
 export function hideElem(elem: HTMLElement): void {
   elem.hidden = true;
 }
